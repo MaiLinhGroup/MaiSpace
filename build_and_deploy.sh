7aacfc84bb19d928366da6e32ba162ef70ee0c6a
Mai Linh Nguyen <mailinh.nguyen@bcgdv.com>
2019-06-12 17:30:30 +0200

    add build and deploy step

---
+++ build_and_deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# either take message from a param or use a date
+COMMIT_MSG=${1-"autocommit $(date "+%Y-%m-%d %H:%M:%S")"}
+
+echo "Committing with message: $COMMIT_MSG"
+
+# removes old artifacts in public/
+rm -rf public
+
+git add --all
+git ci -m "$COMMIT_MSG"
+git push origin master
